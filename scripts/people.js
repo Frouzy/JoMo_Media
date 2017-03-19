4666821301a66f70cb061c122960d82166a178a5
Franka Rolvink <frolvink@hotmail.comscarlet:git config --global user-name Franka>
2017-03-19 21:51:45 +0100

    Create folder structure

---
+++ scripts/people.js
@@ -0,0 +1,28 @@
+
+$(document).ready(function () {
+	
+	$("#dropbtnMember1").click(function(){ 
+        $("#text1").slideToggle("1000");
+    });
+
+$("#dropbtnMember2").click(function(){ 
+        $("#text2").slideToggle("1000");
+    });
+
+$("#dropbtnMember3").click(function(){ 
+        $("#text3").slideToggle("1000");
+    });
+
+
+$(document).ready(function () {
+    $("#header").hide();
+    $("#header").slideDown(3000);
+    $("h2").hide();
+    $("h2").fadeIn(4000);
+     $("h4").hide();
+    $("h4").fadeIn(5000);
+   
+});
+
+});
+
---
+++ scripts/work.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function () {
+    $("#header1").hide();
+    $("#header1").slideDown(3000);
+    $("h2").hide();
+    $("h2").fadeIn(4000);
+     $("h4").hide();
+    $("h4").fadeIn(5000);
+ 
+   
+});
