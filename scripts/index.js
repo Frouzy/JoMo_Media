213a29b8c2844ae02192a9eb20d7f6e5b16d7a11
Franka Rolvink <frolvink@hotmail.comscarlet:git config --global user-name Franka>
2017-04-21 11:32:41 +0200

    Fixed #1

--- scripts/index.js
+++ scripts/index.js
@@ -1,37 +1,21 @@
 //Carousel
-$(document).ready(function () {
 
-    var text = ['We tell stories','We write them','We make it digital','With a vintage touch']
-    var images = ['url(../images/slide1.jpg)','url(../images/work4.jpg)','url(../images/slide3.jpg)','url(../images/slide4.jpg)']
-    var numImages = images.length;
+$(document).ready(function () {
     var index = 0;
+    var slides = $('.slide');
+    var numImages = slides.length;
+
+    $(".slide").hide();
+    $("#slideOne").show();
 
     function carousel() {
-        $('#titleSlide').html(text[index]);  
-        $('#sectieSlide').css('background-image', images[index]);  
-        $('#sectieSlide').css('background-size', 'cover'); 
-        $('#sectieSlide').css('background-position', 'center center');   
+        $(slides[index]).hide();
         index = (index + 1) % numImages;
+        $(slides[index]).show();
     }
 
     timer = setInterval(carousel, 3000);
 });
-
-//  $(document).ready(function () {
-
-//     var slides = $(".sectie");
-//     var numImages = slides.length;
-//     var index = 0;
-
-//     function carousel() {
-//         slides[index].animate({"right": "2000px"});
-//         slides[index].css{}  jquery set elemnt width
-
-//         index = (index + 1) % numImages;
-//     }
-
-//     timer = setInterval(carousel, 3000);
-// });
 
 
 
